b701821a16415fbe754bfbeb406c61cc66883581
0x681 <skepta978@gmail.com>
2026-06-25 14:25:34 +0300

    Update loader URL for specific game PlaceId

--- main.lua
+++ main.lua
@@ -6,5 +6,5 @@ getgenv().Mystic_LOADED = true;
 if (game.PlaceId == 5938036553) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/45319fe3b62f95e8068533193ca78e29.lua"))();
 elseif (game.PlaceId == 17887390746) then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/4dcd622dca762dbbc38a2c4b02676f37.lua"))();
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/058aa7a51c7d19078b10f41fcbf5b94f.lua"))();
 end;
