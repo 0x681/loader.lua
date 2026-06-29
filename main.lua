8dd185e9f20d93fc1d07760b1a32a3c81785b72d
0x681 <skepta978@gmail.com>
2026-06-29 03:02:13 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,6 +5,6 @@ getgenv().Mystic_LOADED = true;
 
 if (game.PlaceId == 5938036553) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/45319fe3b62f95e8068533193ca78e29.lua"))();
-elseif (game.PlaceId == 17887390746) then
+elseif (game.PlaceId == 120189115846709) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/058aa7a51c7d19078b10f41fcbf5b94f.lua"))();
 end;
