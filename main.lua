9ed383e44e176b69f80f5f99fa5391b1712a168f
0x681 <skepta978@gmail.com>
2026-08-07 13:59:25 +0300

    Update game.PlaceId condition for Lua loader

--- main.lua
+++ main.lua
@@ -7,6 +7,6 @@ if (game.PlaceId == 5938036553) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/45319fe3b62f95e8068533193ca78e29.lua"))();
 elseif (game.PlaceId == 120189115846709) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/058aa7a51c7d19078b10f41fcbf5b94f.lua"))();
-elseif (game.PlaceId == 120189115846709) then
+elseif (game.PlaceId == 142823291) then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/6f90d7bed2bea0e10e97abf274e64bb7.lua"))()
 end;
