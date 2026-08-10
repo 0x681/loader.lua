0f7900198e74259f9d39a9fc49c8beda2ac245a4
0x681 <skepta978@gmail.com>
2026-08-10 22:35:58 +0300

    Refactor conditional for PlaceId checks in main.lua

--- main.lua
+++ main.lua
@@ -8,6 +8,8 @@ if game.PlaceId == 5938036553 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/45319fe3b62f95e8068533193ca78e29.lua"))()
 elseif game.PlaceId == 120189115846709 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/058aa7a51c7d19078b10f41fcbf5b94f.lua"))()
-elseif game.PlaceId == 142823291 or game.PlaceId == 636649648 then
+elseif game.PlaceId == 142823291
+    or game.PlaceId == 636649648
+    or game.PlaceId == 335132309 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/6f90d7bed2bea0e10e97abf274e64bb7.lua"))()
 end
